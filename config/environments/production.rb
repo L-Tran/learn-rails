62980ccb7d22473a2c4279b8cad49ffe12e4a876
Long Tran <sk.ltran@gmail.com>
2014-12-09 17:20:00 -0800

    ...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,16 +78,16 @@ Rails.application.configure do
 
   # email enabled in production
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address:"smtp.gmail.com",
     port: 587,
-    domain: Rails.application.secrets.domain_name,
+    domain: ENV["DOMAIN_NAME"],
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
-}
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
